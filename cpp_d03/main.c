dcfb404ecf4615a3a65884c03c2fe25e29ca727c
Jaunet Nathan <Navino16@users.noreply.github.com>
2017-01-06 15:24:42 +0100

    Ex09

--- cpp_d03/main.c
+++ cpp_d03/main.c
@@ -5,7 +5,7 @@
 ** Login   <jaunet_n@epitech.net>
 **
 ** Started on  Fri Jan  6 09:35:32 2017 Nathan Jaunet
-** Last update Fri Jan 06 15:07:32 2017 Nathan Jaunet
+** Last update Fri Jan 06 15:18:58 2017 Nathan Jaunet
 */
 
 #include "String.h"
@@ -27,7 +27,7 @@ int	main(void)
   //Ex01
   String ex01_1;
   String ex01_2;
-  printf("\n---- Ex01 ----\n");
+  printf("\n---- Ex01 Set/Unset ----\n");
   StringInit(&ex01_1, "Hello, ");
   StringInit(&ex01_2, "World !!");
   printf("Initial values :\nValue 1 : %s\nValue 2: %s\n", ex01_1.str, ex01_2.str);
@@ -41,7 +41,7 @@ int	main(void)
   //Ex02
   String ex02_1;
   String ex02_2;
-  printf("\n---- Ex02 ----\n");
+  printf("\n---- Ex02 Append ----\n");
   StringInit(&ex02_1, "Hello, ");
   StringInit(&ex02_2, "World !!");
   printf("Initial values :\nValue 1 : %s\nValue 2: %s\n", ex02_1.str, ex02_2.str);
@@ -54,7 +54,7 @@ int	main(void)
 
   //Ex03
   String ex03_1;
-  printf("\n---- Ex03 ----\n");
+  printf("\n---- Ex03 CharAt ----\n");
   StringInit(&ex03_1, "Test char !");
   printf("Original value : %s\n", ex03_1.str);
   printf("Char at index %d : %c\n", 5, ex03_1.at(&ex03_1, 5));
@@ -62,7 +62,7 @@ int	main(void)
 
   //Ex04
   String ex04_1;
-  printf("\n---- Ex04 ----\n");
+  printf("\n---- Ex04 Clear ----\n");
   StringInit(&ex04_1, "Test string");
   printf("Original value : %s\n", ex04_1.str);
   ex04_1.clear(&ex04_1);
@@ -71,7 +71,7 @@ int	main(void)
 
   //Ex05
   String ex05_1;
-  printf("\n---- Ex05 ----\n");
+  printf("\n---- Ex05 Size ----\n");
   StringInit(&ex05_1, "Test string yolo");
   printf("Expected value : %d\n", 16);
   printf("Your value : %d\n", ex05_1.size(&ex05_1));
@@ -79,7 +79,7 @@ int	main(void)
 
   //Ex06
   String ex06_1;
-  printf("\n---- Ex06 ----\n");
+  printf("\n---- Ex06 Compare ----\n");
   StringInit(&ex06_1, "Test string yolo");
   printf("With same String :\n\tReturn value : %d\n", ex06_1.compare_s(&ex06_1, &ex06_1));
   printf("With differents String :\n\tReturn value : %d\n", ex06_1.compare_c(&ex06_1, "sdqjhkqsldsql"));
@@ -88,7 +88,7 @@ int	main(void)
   //Ex07
   String ex07_1;
   char *str;
-  printf("\n---- Ex07 ----\n");
+  printf("\n---- Ex07 Copy ----\n");
   StringInit(&ex07_1, "Test string yolo");
   str = malloc(sizeof(char) * 7);
   printf("Initial String : %s\n", ex07_1.str);
@@ -99,11 +99,21 @@ int	main(void)
 
   //Ex08
   String ex08_1;
-  printf("\n---- Ex08 ----\n");
+  printf("\n---- Ex08 GetStr ----\n");
   StringInit(&ex08_1, "Blackbeard is OP");
   printf("Initial String : %s\n", ex08_1.str);
   printf("Result String : %s\n", ex08_1.c_str(&ex08_1));
   StringDestroy(&ex08_1);
+
+  //Ex09
+  String ex09_1;
+  printf("\n---- Ex09 Empty ----\n");
+  StringInit(&ex09_1, "");
+  if (ex09_1.empty(&ex09_1) == 1)
+    printf("This String is empty\n");
+  else
+    printf("Error, this String is not empty\n");
+  StringDestroy(&ex09_1);
 
   return (0);
 }
